deb91e49af1c63833fc39cff67e66136f6960da1
chaima31 <chaimaanajali@gmail.com>
2023-11-30 14:37:47 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppareilsComponent } from './appareils/appareils.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { UpdateCategorieComponent } from './update-categorie/update-categorie.component';
+import { UpdateAppareilComponent } from './update-appareil/update-appareil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "appareils", component : AppareilsComponent},
+  {path : "categories", component : CategoriesComponent},
+  {path : "update-categorie/:id", component : UpdateCategorieComponent},
+  {path : "update-appareil/:id", component : UpdateAppareilComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppareilsComponent } from './appareils/appareils.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { UpdateCategorieComponent } from './update-categorie/update-categorie.component';
+import { UpdateAppareilComponent } from './update-appareil/update-appareil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppareilsComponent,
+    CategoriesComponent,
+    UpdateCategorieComponent,
+    UpdateAppareilComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
